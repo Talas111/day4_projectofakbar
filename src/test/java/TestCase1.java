d96f20cfbeb9e2e1b2b98a806fd108ce149a97b9
Shirin <shirkopai@gmail.com>
2021-05-27 14:28:31 -0400

    reportinng about the finish line

--- src/test/java/TestCase1.java
+++ src/test/java/TestCase1.java
@@ -2,5 +2,6 @@ public class TestCase1 {
     public static void main(String[] args) {
 
         // i start working on TS1 but  im having a little trouble to test smoke test
+        System.out.println("APrtually work is done will be reday by tomorow 05/28/2021 at 8 am");
     }
 }
