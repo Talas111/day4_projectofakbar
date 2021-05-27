df834e92413141777b85d226f75fc8226acdc21d
Shirin <shirkopai@gmail.com>
2021-05-27 13:41:43 -0400

    some notes for future work

--- src/test/java/BranchingPractice.java
+++ src/test/java/BranchingPractice.java
@@ -1,5 +1,10 @@
 public class BranchingPractice {
     public static void main(String[] args) {
         //some new notes
+
+        // enabled version contorl
+        //added .giignore folder
+        //i will be trackingb al this file
+        //and going to commit and push to remote 
     }
 }
