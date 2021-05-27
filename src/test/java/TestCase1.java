5c0def61448b335f40d043b456bb3fbd1198446e
Shirin <shirkopai@gmail.com>
2021-05-27 14:29:56 -0400

    work report final one

--- src/test/java/TestCase1.java
+++ src/test/java/TestCase1.java
@@ -3,5 +3,6 @@ public class TestCase1 {
 
         // i start working on TS1 but  im having a little trouble to test smoke test
         System.out.println("APrtually work is done will be reday by tomorow 05/28/2021 at 8 am");
+        System.out.println("repoting about test   case 1 its all complited and i didnt find any ussie which needs to be resolve");
     }
 }
