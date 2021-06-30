18a2b8fd674b5faa51239b6b3dee70d39fac0dd6
Talas111 <shirkopai@gmail.com>
2021-06-30 19:46:52 -0400

    learning

--- src/test/java/day5_1/Test.java
+++ src/test/java/day5_1/Test.java
@@ -4,6 +4,7 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("Hello world" );
         System.out.println("Adding  more view");
+        System.out.println("the most efficient class is just to watch short videos");
     }
 }
 
