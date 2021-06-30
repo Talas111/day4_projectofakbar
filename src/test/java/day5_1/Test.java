566736218fb060fbe3dc6ab4fdb998ad0f654617
Talas111 <shirkopai@gmail.com>
2021-06-30 19:32:16 -0400

    test case 3 is done working

---
+++ src/test/java/day5_1/Test.java
@@ -0,0 +1,9 @@
+package day5_1;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Hello world" );
+        System.out.println("Adding  more view");
+    }
+}
+
