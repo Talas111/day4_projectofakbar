8d61973876be4c11344e9589c21fe7747ce00812
Talas111 <shirkopai@gmail.com>
2021-06-30 20:25:06 -0400

    job

--- src/test/java/day6/Example.java
+++ src/test/java/day6/Example.java
@@ -4,5 +4,6 @@ public class Example {
     public static void main(String[] args) {
         System.out.println("soufhgda");
         System.out.println("need know git so bad");
+        System.out.println("hope i will fnish this course and get vreally nice jovb");
     }
 }
