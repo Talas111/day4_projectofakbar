a91507bf10940480157ded3eab06b9aee52cbeb8
Talas111 <shirkopai@gmail.com>
2021-06-30 20:24:30 -0400

    afain

--- src/test/java/day6/Example.java
+++ src/test/java/day6/Example.java
@@ -3,5 +3,6 @@ package day6;
 public class Example {
     public static void main(String[] args) {
         System.out.println("soufhgda");
+        System.out.println("need know git so bad");
     }
 }
